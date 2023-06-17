8752653b786d09f4358648f04a71da8ccd6f000e
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-17 16:34:20 +0530

    Backend App-done

--- src/main.js
+++ src/main.js
@@ -3,19 +3,40 @@ import express from "express";
 
 const app = express();
 
-async function main() {
+async function addRecord(req, res) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
 
   const db = client.db("vishal");
   const messageColl = db.collection("message");
-  let inputDoc = { message: "thank you God from vishal" };
+  let inputDoc = { message: req.query.message || "DEFAULT" };
   await messageColl.insertOne(inputDoc);
 
   await client.close();
-  console.log("RecordAdded");
-}
-app.get("/main", main);
+  //   console.log("RecordAdded");
+  {
+    /**String response */
+  }
+  //   res.send("Record Added");
 
-main();
-app.listen(5000);
+  //{/**JSON RESPONSE */}
+  res.json({ opr: "success" });
+}
+
+async function findAllMessage(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("vishal");
+  const messageColl = db.collection("message");
+
+  let list = await messageColl.find().toArray();
+
+  await client.close();
+  res.json(list);
+}
+//https://localhost:4000/addRecord
+app.get("/addRecord", addRecord);
+app.get("/findAll", findAllMessage);
+
+app.listen(4000);
